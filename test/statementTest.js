ee3078ed43f82cc6b332614124582bf0f572f912
你的用户名 <997915421@qq.com>
2020-08-27 14:40:02 +0800

    [Refactoring Exercise][Fennie] test: Case4 BigCo has three performance and the audience more than 30

--- test/statementTest.js
+++ test/statementTest.js
@@ -115,3 +115,34 @@ test('Case3 BigCo has one performance hamlet and the audience is 31', t => {
     //then
     t.is(result, expectResult);
 });
+
+test('Case4 BigCo has three performance and the audience more than 30', t => {
+    //given
+    const invoice = {
+        'customer': 'BigCo',
+        'performances': [
+            {
+                'playID': 'hamlet',
+                'audience': 55,
+            },
+            {
+                'playID': 'as-like',
+                'audience': 35,
+            },
+            {
+                'playID': 'othello',
+                'audience': 40,
+            },
+        ],
+    };
+    //when
+    const result = statement(invoice, plays);
+    const expectResult = 'Statement for BigCo\n'
+        + ` Hamlet: $650.00 (55 seats)\n`
+        + ` As You Like It: $580.00 (35 seats)\n`
+        + ` Othello: $500.00 (40 seats)\n`
+        + `Amount owed is $1,730.00\n`
+        + `You earned 47 credits \n`;
+    //then
+    t.is(result, expectResult);
+});
