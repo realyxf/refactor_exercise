e1dcc49aef9ec9a4099c1b9f35494d4633ea9042
你的用户名 <997915421@qq.com>
2020-08-27 17:42:47 +0800

    [Refactoring Exercise][Fennie] feature： add printStatementHtml() and htmlStatement()

--- src/statement.js
+++ src/statement.js
@@ -66,10 +66,28 @@ function printStatementText(data) {
     return result;
 }
 
-function statement(invoice, plays) {
+function printStatementHtml(data){
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += '<table>\n';
+    result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+    for (let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td><td>${priceFormat(perf.amount)}</td></tr>\n`;
+    }
+    result += '</table>\n';
+    result += `<p>Amount owed is <em>${priceFormat(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.volumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
+const htmlStatement = (invoice, plays) => {
+    return printStatementHtml(createStatementData(invoice, plays));
+};
+
+const statement = (invoice, plays) => {
     return printStatementText(createStatementData(invoice, plays));
 }
 
 module.exports = {
     statement,
+    htmlStatement,
 };
