930751a615a7d20aab56991f70c151eff8d5cb7e
你的用户名 <997915421@qq.com>
2020-08-27 15:35:30 +0800

    [Refactoring Exercise][Fennie] refactor： extract priceFormat() from statement()

--- src/statement.js
+++ src/statement.js
@@ -1,12 +1,16 @@
-function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
+function priceFormat() {
+  return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format;
+}
+
+function statement (invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = priceFormat();
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
@@ -37,7 +41,6 @@ function statement (invoice, plays) {
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
-
   return result;
 }
 
