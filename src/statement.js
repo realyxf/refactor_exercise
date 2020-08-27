6509fa9cfdef8e9a7a77e342306f23dce67acf81
你的用户名 <997915421@qq.com>
2020-08-27 17:05:10 +0800

    [Refactoring Exercise][Fennie] refactor： extract createStatementData() from statement()

--- src/statement.js
+++ src/statement.js
@@ -6,49 +6,70 @@ function priceFormat() {
     }).format;
 }
 
-function calculateAmount(plays, perf) {
-    const play = plays[perf.playID];
+function calculateAmount(performances) {
     let thisAmount = 0;
-    switch (play.type) {
+    switch (performances.play.type) {
         case 'tragedy':
             thisAmount = 40000;
-            if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
+            if (performances.audience > 30) {
+                thisAmount += 1000 * (performances.audience - 30);
             }
             break;
         case 'comedy':
             thisAmount = 30000;
-            if (perf.audience > 20) {
-                thisAmount += 10000 + 500 * (perf.audience - 20);
+            if (performances.audience > 20) {
+                thisAmount += 10000 + 500 * (performances.audience - 20);
             }
-            thisAmount += 300 * perf.audience;
+            thisAmount += 300 * performances.audience;
             break;
         default:
-            throw new Error(`unknown type: ${play.type}`);
+            throw new Error(`unknown type: ${performances.play.type}`);
     }
-    return {play, thisAmount};
+    return thisAmount;
 }
 
-function calculateCredits(volumeCredits, perf, play) {
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+function calculateCredits(invoice, plays) {
+    let volumeCredits = 0;
+    for(let perf of invoice.performances){
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ('comedy' === plays[perf.playID].type) volumeCredits += Math.floor(perf.audience / 5);
+    }
     return volumeCredits;
 }
 
+function calculateTotalAmount(result){
+    return result.performances.reduce((total, curPerformance) => total + curPerformance.amount, 0);
+}
+
+function createStatementData(invoice, plays){
+    const  result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(function (curPerformance){
+        const result = Object.assign({},curPerformance);
+        result.play = plays[result.playID];
+        result.amount = calculateAmount(result);
+        return result;
+    });
+    result.totalAmount = calculateTotalAmount(result);
+    result.volumeCredits = calculateCredits(invoice, plays);
+    return result;
+}
+
 function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
+    // let totalAmount = 0;
+    // let volumeCredits = 0;
+    let result = `Statement for ${createStatementData(invoice, plays).customer}\n`;
     const format = priceFormat();
-    for (let perf of invoice.performances) {
-        let {play, thisAmount} = calculateAmount(plays, perf);
-        volumeCredits = calculateCredits(volumeCredits, perf, play);
+    for (let perf of createStatementData(invoice, plays).performances) {
+        //let {play, thisAmount} = calculateAmount(plays, perf);
+        // let thisAmount = calculateAmount(invoice.performances);
+        // volumeCredits = calculateCredits(volumeCredits, perf, play);
         //print line for this order
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        result += ` ${perf.play.name}: ${format(perf.amount / 100)} (${perf.audience} seats)\n`;
+        //totalAmount += thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
-    result += `You earned ${volumeCredits} credits \n`;
+    result += `Amount owed is ${format(createStatementData(invoice, plays).totalAmount / 100)}\n`;
+    result += `You earned ${createStatementData(invoice, plays).volumeCredits} credits \n`;
     return result;
 }
 
